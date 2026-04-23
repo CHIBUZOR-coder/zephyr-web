e7a59c9d2af2ed52a07d591422bdc45e6a34d3a2
Chibuzor <chibuzormekalam@gmail.com>
2026-04-23 19:29:32 +0100

    added main changes2

--- src/core/query/authClient.ts
+++ src/core/query/authClient.ts
@@ -1,7 +1,7 @@
 import { useAuthStore } from "../../features/auth/auth.store";
 
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3002";
-// export const API_BASE = "https://zephyr-np09.onrender.com";
+// export const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3002";
+export const API_BASE = "https://zephyr-np09.onrender.com";
 
 let isRefreshing = false;
 
